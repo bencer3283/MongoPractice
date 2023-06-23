cf99ee06540be6b1935e3bc6fa10ad116d8cf46f
ChengPoSheng <bencer@outlook.com>
2023-06-23 14:26:21 +0800

    find people

--- myApp.js
+++ myApp.js
@@ -33,7 +33,14 @@ const createManyPeople = (arrayOfPeople, done) => {
 };
 
 const findPeopleByName = (personName, done) => {
-  done(null /*, data*/);
+  Person.find({name: personName}).then(
+    (doc) => {
+      done(null, doc);
+    },
+    (err) => {
+      done(err);
+    }
+  );
 };
 
 const findOneByFood = (food, done) => {
