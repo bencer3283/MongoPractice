ad830ae69770babc164f5b2b72638d9617627947
ChengPoSheng <bencer@outlook.com>
2023-06-29 18:26:22 +0800

    log header find by food

--- server.js
+++ server.js
@@ -171,7 +171,7 @@ router.post("/find-one-by-food", function (req, res, next) {
   let t = setTimeout(() => {
     next({ message: "timeout" });
   }, TIMEOUT);
-  console.log(req);
+  console.log(req.header);
   let p = new Person(req.body);
   p.save(function (err, pers) {
     if (err) {
